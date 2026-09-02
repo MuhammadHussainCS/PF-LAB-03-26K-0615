3e645acfc167b39ac033e49ee384f0afc775e800
Muhammad Hussain <mhussain.dev.cs@gmail.com>
2026-09-03 01:33:32 +0500

    Update task1 datatypes

--- task1_datatypes.c
+++ task1_datatypes.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdbool.h>
+//pf lab home task 03
 int main(){
     printf("===============================================================\n");
     printf("Int data type usage\n");
